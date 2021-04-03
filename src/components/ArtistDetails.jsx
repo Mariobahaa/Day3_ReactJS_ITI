ffed368553f06b1ff69317e2b7e8d9b79f3196ca
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-04-03 05:22:23 +0200

    using Hooks

--- src/components/ArtistDetails.jsx
+++ src/components/ArtistDetails.jsx
@@ -1,75 +1,15 @@
-import { Component } from "react";
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-
-export default class ArtistDetails extends Component{
-    constructor(){
-        super();
-        this.state ={
-            artist : {},
-            loaded : false,
-        }
-    }
-
-    componentDidMount(){
-        fetch(`http://localhost:1998/artists/`+this.props.match.params.id).then(
-            (data)=>{
-                return data.json();
-            }).then((d)=>
-    
-                this.setState({artist:d, loaded:true})).catch((e)=> console.error(e));
-    }
-
-    LoadAlbums(){
-        if(this.state.loaded)
-        return this.state.artist.albums.map((alb)=>
-        (<img src={`/images/albums/${alb.cover}.jpg`} className= " offset-1 col-5 h-50" alt={alb.cover}/>))
-        else return;
-    }
-
-
-    // eslint-disable-next-line react/require-render-return
-    render(){
-        if(this.state.loaded)
-        return(
-        <div >
-        <Link to= "/artists/"> 
-        <div className="row">
-            
-            <h1 className= "text-center col-12">Music DB</h1>
-        </div>
-        </Link>
-        <div className = "row text-center">
-        <img src = {`/images/covers/${this.state.artist.cover??""}.jpg`}
-        className = "col-4 offset-4" alt={this.state.artist.cover}></img>
-        <h5>{this.state.artist.name??""}</h5>
-        <p>{this.state.artist.bio??""}</p>
-        <div className="row">
-        {
-            
-            this.LoadAlbums()
-        }
-        </div>
-        </div>
-        
-
-    </div>
-        )
-    else return <h1>No Data</h1>
-    }
-}
-
-/*import { useState, useEffect } from 'react';
 
 
 const ArtistDetails = ({match})=>{
-    //console.error(match);
+    console.error(match);
     let [artist, setArtist] = useState({});
     let [reload, setReload] = useState(false);
     
 
     
-// const [, updateState] = useState();
-// const forceUpdate = useCallback(() => updateState({}), []);
+
 
     useEffect(()=>{
     fetch(`http://localhost:1998/artists/`+match.params.id).then(
@@ -87,11 +27,10 @@ const ArtistDetails = ({match})=>{
         {
             
            console.log(artist);
-            return artist.albums.map((alb)=> {
-                console.log(alb.cover);
-            return 
-            (<img src={`images/albums/${alb.cover}.jpg`} alt={alb.cover}/>);
-        })
+            return artist.albums.map((alb)=> 
+            
+            (<img src={`/images/albums/${alb.cover}.jpg`} className= "col-5 h-50" alt={alb.cover}/>))
+        
         }
 
         
@@ -102,19 +41,23 @@ const ArtistDetails = ({match})=>{
     return(
         <div >
             
+           <Link to= "/artists/"> 
             <div className="row">
-                <h1 className= "text-center col-12">Music DB</h1>
-            </div>
+            
+            <h1 className= "text-center col-12">Music DB</h1>
+            </div>       
+           </Link>
             <div className = "row text-center">
-            <img src = {`images/covers/${artist.cover}.jpg`}
+            <img src = {`/images/covers/${artist.cover}.jpg`}
             className = "col-4 offset-4" alt="img"></img>
             <h5>{artist.name}</h5>
             <p>{artist.bio}</p>
-            <div>
-            {
-                
-                LoadAlbums()
-            }
+            <div className= "row">
+            
+              <div className="offset-1 col-10">  
+                {LoadAlbums()}
+                </div>
+            
             </div>
             </div>
             
@@ -124,4 +67,66 @@ const ArtistDetails = ({match})=>{
    else return <h1>No Data</h1>
 }
 
-export default ArtistDetails*/
+export default ArtistDetails
+
+
+
+// import { Component } from "react";
+// import { Link } from "react-router-dom";
+
+// export default class ArtistDetails extends Component{
+//     constructor(){
+//         super();
+//         this.state ={
+//             artist : {},
+//             loaded : false,
+//         }
+//     }
+
+//     componentDidMount(){
+//         fetch(`http://localhost:1998/artists/`+this.props.match.params.id).then(
+//             (data)=>{
+//                 return data.json();
+//             }).then((d)=>
+    
+//                 this.setState({artist:d, loaded:true})).catch((e)=> console.error(e));
+//     }
+
+//     LoadAlbums(){
+//         if(this.state.loaded)
+//         return this.state.artist.albums.map((alb)=>
+//         (<img src={`/images/albums/${alb.cover}.jpg`} className= " offset-1 col-5 h-50" alt={alb.cover}/>))
+//         else return;
+//     }
+
+
+//     // eslint-disable-next-line react/require-render-return
+//     render(){
+//         if(this.state.loaded)
+//         return(
+//         <div >
+//         <Link to= "/artists/"> 
+//         <div className="row">
+            
+//             <h1 className= "text-center col-12">Music DB</h1>
+//         </div>
+//         </Link>
+//         <div className = "row text-center">
+//         <img src = {`/images/covers/${this.state.artist.cover??""}.jpg`}
+//         className = "col-4 offset-4" alt={this.state.artist.cover}></img>
+//         <h5>{this.state.artist.name??""}</h5>
+//         <p>{this.state.artist.bio??""}</p>
+//         <div className="row">
+//         {
+            
+//             this.LoadAlbums()
+//         }
+//         </div>
+//         </div>
+        
+
+//     </div>
+//         )
+//     else return <h1>No Data</h1>
+//     }
+// }
